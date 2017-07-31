a331e5b67b7083eb1932ecc998562d10be8303b7
hyunjung.han <han.hyunjung17@gmail.com>
2017-07-31 23:53:23 +0900

    added todo app's components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoListItemComponent,
+    TodoListHeaderComponent
   ],
   imports: [
     BrowserModule
